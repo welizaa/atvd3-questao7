d9c947b00ba148f479724327cf7b916ef0bce674
welizaa <wmss6@aluno.ifal.edu.br>
2024-12-23 18:12:44 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
+let palavra = prompt("Digite uma palavra para reverter:");
 
+let palavrainvertida = "";
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+    palavrainvertida += palavra[i];
+}
+
+
+alert("A palavra invertida e: " + palavrainvertida);
